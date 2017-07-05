519ffcc3d5bbac19879a32ce5a44dd4d786c26fd
kwokhung <chu_kwokhung@yahoo.com.hk>
2017-07-05 17:19:20 +0800

    init

---
+++ Wifi.h
@@ -0,0 +1,20 @@
+#ifndef Wifi_h
+#define Wifi_h
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include "OLed.h"
+
+class Wifi
+{
+public:
+  Wifi(ESP8266WebServer *server, OLed *oLed);
+  void setup();
+
+private:
+  ESP8266WebServer *server;
+  OLed *oLed;
+  String connectInfo;
+};
+
+#endif
