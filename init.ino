c4ede6aa75f74bccd6fecf0ce52c6467c047c7b0
kwokhung <chu_kwokhung@yahoo.com.hk>
2017-08-29 11:03:04 +0800

    init

--- init.ino
+++ init.ino
@@ -1,8 +1,14 @@
+#include <ArduinoJson.h>
+#include <EEPROM.h>
+#include <SSD1306.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <PubSubClient.h>
+
 #include "Mqtt.h"
 #include "OLed.h"
 #include "Led.h"
-#include <EEPROM.h>
-#include <SSD1306.h>
+#include "Wifi.h"
 
 #define RX0 3 // GPIO3 / RX / D0
 #define TX0 1 // GPIO1 / TX / D1
@@ -12,22 +18,17 @@
 
 #define OnboardLED 2 // GPIO2 / E4 / D9
 
+const char *mqtt_server = "mbltest01.mqtt.iot.gz.baidubce.com";
+
 Led led(RLED, GLED, BLED);
 
 SSD1306 display(0x3c, SDA, SCL);
 QRcode qrcode(&display);
 OLed oLed(&display, &qrcode);
 
-#include <ArduinoJson.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#include "Wifi.h"
-#include <PubSubClient.h>
-
-const char *mqtt_server = "mbltest01.mqtt.iot.gz.baidubce.com";
-
 ESP8266WebServer server(80);
 Wifi wifi(&server, &oLed);
+
 WiFiClient espClient;
 //WiFiClientSecure espClient;
 PubSubClient client(espClient);
