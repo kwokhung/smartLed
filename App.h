55eb8e1a6bfc1fce8d296d5f6eac8a56b0260e4b
kwokhung <chu_kwokhung@yahoo.com.hk>
2017-08-30 11:59:29 +0800

    init

--- App.h
+++ App.h
@@ -1,6 +1,8 @@
 #ifndef App_h
 #define App_h
 
+#define OnboardLED 2 // GPIO2 / E4 / D9
+
 #define RLED 5 // GPIO5 / E1 / D3
 #define GLED 4 // GPIO4 / E2 / D4
 #define BLED 0 // GPIO0 / E3 / D8
@@ -9,9 +11,8 @@
 #define TX0 1 // GPIO1 / TX / D1
 
 #define SCL 14 // GPIO14 / E5 / D5
+#define SCL 14 // GPIO14 / E5 / D5
 #define SDA 12 // GPIO12 / E6 / D6
-
-#define OnboardLED 2 // GPIO2 / E4 / D9
 
 class App
 {
